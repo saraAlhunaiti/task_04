964157e764490a662a51c8ef384360d1765bcf92
saraAlhunaiti <alhunaitisara@gmail.com>
2019-07-23 16:32:59 +0300

    Day 3 task_04 -models-

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+class Restaurant(models.Model):
+	name = models.CharField(max_length =20)
+	description = models.TextField()
+	opening_time = models.TimeField()
+	closing_time =models.TimeField()
 
+
+	"""docstring for ClassName"""
+	
 # Create your models here.
